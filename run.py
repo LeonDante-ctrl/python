0bbfd93c2c5466486fdc5dc164f74395001472f4
Dante.L <dantel@D.L>
2020-09-06 21:23:30 +0300

    implemented curly braces at my lower() function

--- run.py
+++ run.py
@@ -4,14 +4,13 @@ import random
 
 def main():
 
-    while true:
+    while True:
         print("Welcome to your very own password locker")
         print('/n')
-        print("Type in a short code to select your option:to create new user use 'NU':To login to your account use 'LI' or 'EX' to exit current activity")
-        code = input().lower
-        print('\n')
+        print("Type in a short code to select your option:to create new user use 'nu': To login to your account use 'li' or 'ex' to exit current activity")
+        code = input().lower()
 
-        if code == 'NU':
+        if code == 'nu':
             print("create user name please")
             created_user = input()
 
@@ -30,7 +29,6 @@ def main():
 
             else:
                 print(f"Amazing {created_user}! account created")
-                print('\n')
                 print("Head over to login")
                 print("Username please")
                 inputted_user = input()
@@ -46,16 +44,14 @@ def main():
 
             else:
                 print(f"Welcome {inputted_user} to your account")
-                print('\n')
 
-            elif code == 'LI':
+            if code == 'li':
                 print("Welcome buddy")
                 print("Enter username")
                 main_user = input()
 
                 print("Enter password here")
                 main_password = input()
-                print('\n')
 
             while main_user != 'testuser' or main_password != '01234':
                 print("Wrong username or password . Username 'testuser' and password '01234'") #enter username in text form and password as digit details
@@ -64,14 +60,13 @@ def main():
 
                 print("Enter password again")
                 main_password = input()
-                print("\n")
 
             else:
                 print("Login complete")
                 print("\n")
                 print("\n")
 
-            elif code == 'EX':
+            if code == 'ex':
                 break
             else:
                 print("Enter a valid code to proceed")
