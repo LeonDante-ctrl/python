f927d14a606f0d938dab60ac3a2ba29a7511e343
Dante.L <dantel@D.L>
2020-09-06 19:38:42 +0300

    add methods that will define the basic structure of the inputs saving methods

---
+++ you.py
@@ -0,0 +1,16 @@
+class User:
+    '''
+    generates new instance for user
+    '''
+
+    user_list = [] #empty list
+
+    def __init__(self,user_name,pass):
+        self.user_name = user_name
+        self.pass = pass
+
+    def save_use(self):
+        '''
+        save_use method saves saves a new user objects to user_list
+        '''
+        User.user_list.append(self)
